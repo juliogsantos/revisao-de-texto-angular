e22261454bbb1aec6e4ada41b6e9ffadd838565a
Julio Guilherme dos Santos <julio.santos@CAMBAUBA.COM.BR>
2018-08-20 16:01:39 -0300

    Troca do framework CSS - MaterializeCSS e modelagem conceitual do banco de dados

--- service/publicacao-service.js
+++ service/publicacao-service.js
@@ -1,12 +1,12 @@
 angular.module("rev").service('publicacaoService', function($http){
-   /* this.validarLogin = function(user){
+    this.validarLogin = function(user){
         if(user.login == "admin" && user.senha == "admin") {
             console.log("logou");
             return true;
         }
         console.log("n logou");
         return false;
-    };*/
+    };
 });
 
 
